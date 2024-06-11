e1e2bb1136f90a6535ade95db671832a49765178
konstantinefimov <relict.adm@gmail.com>
2024-06-11 22:07:02 +0300

    Add font

--- app/navigations/MainTabNavigation.tsx
+++ app/navigations/MainTabNavigation.tsx
@@ -3,15 +3,35 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MainScreen } from '@/screens/MainScreen';
 import { AnotherScreen } from '@/screens/AnotherScreen';
 import {RegistrationScreen} from "@/screens/RegistrationScreen";
+import {MaterialCommunityIcons} from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 export const MainTabNavigator: React.FC = () => {
 	return (
 		<Tab.Navigator>
-			<Tab.Screen name="Home" component={MainScreen} options={{headerShown: false}}/>
-			<Tab.Screen name="Another" component={AnotherScreen} options={{headerShown: false}} />
-			<Tab.Screen name="Профиль" component={RegistrationScreen} options={{headerShown: false}} />
+			<Tab.Screen name="Home" component={MainScreen} options={{headerShown: false, tabBarIcon: ({color, size}) => (
+					<MaterialCommunityIcons
+						name="home"
+						color={color}
+						size={size}
+					/>
+				),
+			}}/>
+			<Tab.Screen name="Another" component={AnotherScreen} options={{headerShown: false, tabBarIcon: ({color, size}) => (
+					<MaterialCommunityIcons
+						name="account-box"
+						color={color}
+						size={size}
+					/>
+				),}} />
+			<Tab.Screen name="Профиль" component={RegistrationScreen} options={{headerShown: false, tabBarIcon: ({color, size}) => (
+					<MaterialCommunityIcons
+						name="account-box"
+						color={color}
+						size={size}
+					/>
+				),}} />
 		</Tab.Navigator>
 	);
 };
